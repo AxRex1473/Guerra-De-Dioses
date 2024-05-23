a9684b20cb172956478a9322a4930598fff65096
Ivan PM <cdmx2125@amerike.edu.mx>
2024-05-22 20:57:36 -0600

    Testing Anim

--- Rts/Assets/Script/Pause/TestingAnim.cs
+++ Rts/Assets/Script/Pause/TestingAnim.cs
@@ -1,15 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TestingAnim : MonoBehaviour
 {
-    [SerializeField] private PauseGame gameUIref;
+    TMP_Text textMesh;
 
-    //Una vez que termine la animación se ejecuta la Función AnimDone() en el PauseGame.
-    public void TriggerEvent()
+    Mesh mesh;
+
+    Vector3[] vertices;
+
+    [SerializeField] private float _intensity=3.3f;
+
+    // Start is called before the first frame update
+    void Start()
     {
-        gameUIref.AnimDone();
+        textMesh = GetComponent<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textMesh.ForceMeshUpdate();
+        mesh = textMesh.mesh;
+        vertices = mesh.vertices;
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 offset = Wobble(Time.time + i);
+
+            vertices[i] = vertices[i] + offset;
+        }
+
+        mesh.vertices = vertices;
+        textMesh.canvasRenderer.SetMesh(mesh);
+    }
+
+    Vector2 Wobble(float time)
+    {
+        return new Vector2(Mathf.Sin(time * _intensity), Mathf.Cos(time * 2.5f));
     }
 
 }
