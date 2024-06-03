ae894dcb39f68b4ce68ad32845dc7c94f6438c15
Ivan PM <cdmx2125@amerike.edu.mx>
2024-06-03 12:18:50 -0600

    Cambios MenuAnimations Implementado

--- Rts/Assets/Script/Pause/CustomAnimationsMenu.cs
+++ Rts/Assets/Script/Pause/CustomAnimationsMenu.cs
@@ -4,15 +4,73 @@ using UnityEngine;
 
 public class CustomAnimationsMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _canvasPause;
+    [SerializeField] private GameObject _canvasOptions;
+    [SerializeField] private GameObject _canvasGame;
+    [SerializeField] private Animator _pauseAnimator;
+    [SerializeField] private Animator _bgAnimator;
+    //[SerializeField] private Animator _partofPauseAnimator;
+    [SerializeField] private Animator _optionAnim;
+    [SerializeField] private Animator _textOptionAnim;
+    
+
+    public void PauseAnim()
     {
-        
+        _canvasPause.SetActive(true);
+        _canvasGame.SetActive(false);
+        _pauseAnimator.SetTrigger("MenuAction");
+        _bgAnimator.SetTrigger("Fade");
     }
 
-    // Update is called once per frame
-    void Update()
+    //La idea es que se reproduzca la animación, después se lanza un Event Animation en TestingAnim cuándo ese Evento sea triggereado AnimDone se ejecutará.
+    public void Resume()
     {
-        
+        _pauseAnimator.SetTrigger("MenuAction");
+        _bgAnimator.SetTrigger("Fade");
+
     }
+
+    //Se resume el juego y se oculta el Pause Canvas
+    public void ResumeAnimDone()
+    {
+        _canvasPause.SetActive(false);
+        _canvasGame.SetActive(true);
+        Time.timeScale = 1.0f;
+    }
+
+    //Hace la otra animación del menú de pausa para que se cierre y así en Testing Anim de la señal para hacer el OptionShow
+    public void OptionBtn()
+    {
+        _pauseAnimator.SetTrigger("MenuOptions");
+    }
+
+    public void OptionShow()
+    {
+        //Aquí hace animación de ocultar la parte del PauseCanvas y después la animación que muestra las opciones
+
+        _canvasOptions.SetActive(true);
+        // _canvasPartofPause.SetActive(false);
+        _textOptionAnim.SetTrigger("Texting");
+        _optionAnim.SetTrigger("NewOptions");
+
+        //_pauseAnimator.SetTrigger("MenuAction");
+    }
+
+    public void BackToPauseCanvas()
+    {
+        //Se hace la animación de ocultar el OptionAnim, pasamos al código TestinAnim
+        _textOptionAnim.SetTrigger("Texting");
+        _optionAnim.SetTrigger("NewOptions");
+
+    }
+
+    public void OptionDone()
+    {
+        //Se desactiva el canvas Option, se activa la parte del canvas de Pausa y se hace la animación de regreso del canvas de pausa.
+        _canvasOptions.SetActive(false);
+        _pauseAnimator.SetTrigger("MenuOptions");
+        //_partofPauseAnimator.SetTrigger("OptionsChange");
+    }
+
+
 }
--- Rts/Assets/Script/Pause/TestingAnim.cs
+++ Rts/Assets/Script/Pause/TestingAnim.cs
@@ -7,22 +7,24 @@ public class TestingAnim : MonoBehaviour
 {
     //Este código va para DinamicPauseCanvas, para que pueda hacer la función y le diga al objeto con el código PauseGame que ya terminó la animación.
     [SerializeField] private PauseGame gameUIref;
+    [SerializeField] private CustomAnimationsMenu animMenu;
 
     //Una vez que termine la animación se ejecuta la Función AnimDone() en el PauseGame.
     public void AnimationDoDone()
     {
        // if(Opti)
-        gameUIref.AnimDone();
+        //gameUIref.AnimDone();
+        animMenu.ResumeAnimDone();  
     }
 
     public void AnimationPartOfMenuDone()
     {
-        gameUIref.OptionShow();
+        animMenu.OptionShow();
     }
 
     //Termina la animación de Opcion Cerrar nos regresamos a PauseGame
     public void AnimationOptionDone()
     {
-        gameUIref.OptionDone();
+        animMenu.OptionDone();
     }
 }
